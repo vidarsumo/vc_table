ad0a185ba77c4c530fc1f2f207282616dcc2928d
Jane Doe <jane@example.org>
2022-04-26 21:49:28 +0000

    tests

--- 00_scripts/01_extract_tables.R
+++ 00_scripts/01_extract_tables.R
@@ -2,12 +2,32 @@
 
 # 1.0 Setup ----
 library(tidyverse)
-#library(pdftools)
+library(pdftools)
 library(tabulizer)
 
 
 # 2.0 Import data
 
-tables <- extract_tables("00_data/vc_tables.pdf")
+# 2.1 Tabulizer ----
+table_tablulizer_method_1 <- extract_tables("00_data/vc_tables.pdf", method = "decide", output = "data.frame")
+table_tablulizer_method_2 <- extract_tables("00_data/vc_tables.pdf", method = "lattice", output = "data.frame")
+table_tablulizer_method_3 <- extract_tables("00_data/vc_tables.pdf", method = "stream", output = "data.frame")
 
-view(tables[[2]])
+
+# 2.2 PDF tools
+#table_pdf_tools <- pdf_text("00_data/vc_tables.pdf")
+
+
+
+# 3.0 Extract data ----
+
+exctract_fund_type <- function(x) {
+
+}
+
+test_table <- table_tablulizer_method_3 %>%
+    map(as_tibble) %>%
+    map(~ .x %>% pull(FUND) %>% pluck(1))
+
+
+test_table[[1]]
